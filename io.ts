581d571edc705de4caa4d59d48df3753ef79a338
Monte Johnston <johnston.monte@gmail.com>
2018-09-15 11:14:25 -0400

    Clean up io module

--- io.ts
+++ io.ts
@@ -9,17 +9,11 @@ const filePath = process.argv[2]
 function fileContents(file) {
   const filePath = path.resolve(__dirname, file)
 
-  return readFile(filePath, { encoding: 'utf8' })
-    .then(text => {
-      console.log('CONTENT:', text)
-      return text
-    })
-    .catch(err => {
-      const error = `\nSomething went wrong. Here is the error message: \n ${
-        err.message
-      }`
-      throw error
-    })
+  return readFile(filePath, { encoding: 'utf8' }).catch(err => {
+    throw `\nSomething went wrong. Here is the error message: \n ${err.message}`
+  })
 }
+
+function writeToFile(filename) {}
 
 module.exports = { fileContents }
