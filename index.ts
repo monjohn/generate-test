9c216eb1f2d16fd97ff9d4f7e8437c357ea3e84c
Monte Johnston <johnston.monte@gmail.com>
2018-10-29 22:42:18 -0400

    Add path to filename

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
 const parsing = require('./src/parsing.ts')
 const printing = require('./src/printing.ts')
 const io = require('./src/io.ts')
+const path = require('path')
 
 module.exports = () => {
   const args = process.argv.slice(2)
-  const fileName = args[0]
-  io.fileContents(fileName)
+  const filePath = args[0]
+  const fileName = path.basename(filePath)
+
+  io.fileContents(filePath)
     .then(parsing.parse)
     .then(sourceCode => printing.print(sourceCode, fileName))
-    .then(testCode => io.writeToFile(testCode, fileName, 'utf8'))
+    .then(testCode => io.writeToFile(testCode, filePath, 'utf8'))
     .catch(e => {
       console.log(e)
     })
